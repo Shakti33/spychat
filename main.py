c69047b4a7c194d74bf2ee6c9c3449387b37cf66
Shakti Nath Saini <shaktinathsaini.sns@gmail.com>
2017-06-21 14:18:47 +0530

    added

--- main.py
+++ main.py
@@ -66,6 +66,16 @@ def add_status():
     return updated_status_message
 
 
+def print_friends():
+    item_number = 0
+    print '\nYour friends are:\n'
+    for friend in friends:
+        print colored('%d. %s %s aged %d with rating %.2f is online' % (item_number + 1, friend.salutation, friend.name,
+                                                                        friend.age,
+                                                                        friend.rating), 'blue')
+        item_number = item_number + 1
+    return
+
 def add_friend():
 
     new_friend = Spy('','',0,0.0)
@@ -73,7 +83,6 @@ def add_friend():
     new_friend.name = raw_input("Please add your friend's name: ")
     new_friend.salutation = raw_input("Are they Mr. or Ms.?: ")
 
-    new_friend.name = new_friend.salutation + " " + new_friend.name
 
     new_friend.age = raw_input("Age?")
     new_friend.age = int(new_friend.age)
@@ -84,39 +93,28 @@ def add_friend():
     if len(new_friend.name) > 0 and new_friend.age > 12 and new_friend.rating >= spy.rating:
         friends.append(new_friend)
         print 'Friend Added!'
+        print_friends()
     else:
         print 'Sorry! Invalid entry. We can\'t add spy with the details you provided'
 
     return len(friends)
 
 
+
 def remove_friend():
-    item_number = 0
-
-    for friend in friends:
-        print '%d. %s %s aged %d with rating %.2f is online' % (item_number + 1, friend.salutation, friend.name,
-                                                                friend.age,
-                                                                friend.rating)
-        item_number = item_number + 1
-
+    print_friends()
     friend_choice = raw_input("Choose from your friends")
+
 
     friend_choice_position = int(friend_choice) - 1
 
     del friends[friend_choice_position]
-
+    print_friends()
     return len(friends)
 
 
 def select_a_friend():
-    item_number = 0
-
-    for friend in friends:
-        print '%d. %s %s aged %d with rating %.2f is online' % (item_number +1, friend.salutation, friend.name,
-                                                   friend.age,
-                                                   friend.rating)
-        item_number = item_number + 1
-
+    print_friends()
     friend_choice = raw_input("Choose from your friends")
 
     friend_choice_position = int(friend_choice) - 1
@@ -183,10 +181,10 @@ def start_chat(spy):
         show_menu = True
 
         while show_menu:
-            menu_choices = "What do you want to do? \n 1. Add a status update \n 2. Add a friend \n " \
-                           "3. Remove a friend \n 4. Send a secret message \n 5. Read a secret message \n " \
-                           "6. Read Chats from a user \n " \
-                           "7. Close Application \n"
+            menu_choices = "What do you want to do? \n 1. Add a status update \n 2. View friends \n 3. Add a friend \n " \
+                           "4. Remove a friend \n 5. Send a secret message \n 6. Read a secret message \n " \
+                           "7. Read Chats from a user \n " \
+                           "8. Close Application \n"
             menu_choice = raw_input(menu_choices)
 
             if len(menu_choice) > 0:
@@ -195,17 +193,19 @@ def start_chat(spy):
                 if menu_choice == 1:
                     spy.current_status_message = add_status()
                 elif menu_choice == 2:
+                    print_friends()
+                elif menu_choice == 3:
                     number_of_friends = add_friend()
                     print 'You have %d friends' % (number_of_friends)
-                elif menu_choice == 3:
+                elif menu_choice == 4:
                     number_of_friends = remove_friend()
                     print '\nFriend removed!'
                     print 'You have %d friends' % (number_of_friends) , '\n'
-                elif menu_choice == 4:
-                    send_message()
                 elif menu_choice == 5:
-                    read_message()
+                    send_message()
                 elif menu_choice == 6:
+                    read_message()
+                elif menu_choice == 7:
                     read_chat_history()
                 else:
                     show_menu = False
